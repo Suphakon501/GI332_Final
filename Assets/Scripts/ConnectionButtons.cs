ff1d59f22dc5c48ad0e4a5cd8ec9e068e265a47f
rattikan-mata <rattikan.mata@bumail.net>
2026-03-03 19:57:46 +0700

    UI and Script - Main Menu

--- Assets/Scripts/ConnectionButtons.cs
+++ Assets/Scripts/ConnectionButtons.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 public class ConnectionButtons : MonoBehaviour
 {
+    public GameObject connectionCanvas;
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        connectionCanvas.SetActive(false);
     }
 
     public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        connectionCanvas.SetActive(false);
     }
 
 }
